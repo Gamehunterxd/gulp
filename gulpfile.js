6705b677f0b3ce21b518b0f4d061193043782edc
Gamehunterxd <Gamehunterxd@hotmail.dk>
2019-10-03 01:37:35 +0200

    Added autoprefixer and minified css

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,15 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass");
 const sourcemaps =require("gulp-sourcemaps");
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const cssnano = require("cssnano");
 
 function styles() {
     return(gulp.src("css/*.scss")
             .pipe(sourcemaps.init())
             .pipe(sass())
+            .pipe(postcss([autoprefixer({grid: true}), cssnano()]))
             .pipe(sourcemaps.write("."))
             .pipe(gulp.dest("css"))
     );
