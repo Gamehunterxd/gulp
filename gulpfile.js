a200239f2ddcd9eea7dec17ec4d4f8a19134af7b
Gamehunterxd <Gamehunterxd@hotmail.dk>
2019-10-03 01:22:52 +0200

    Added sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,12 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass");
+const sourcemaps =require("gulp-sourcemaps");
 
 function styles() {
     return(gulp.src("css/*.scss")
+            .pipe(sourcemaps.init())
             .pipe(sass())
+            .pipe(sourcemaps.write("."))
             .pipe(gulp.dest("css"))
     );
 
